86f95c38f048ce7134d035e1f8ef26c5e9a974c3
Janet Scanlon <janet.Lscanlon@gmail.com>
2024-04-25 14:28:56 -0500

    boilerplate and db set up

---
+++ server/routes/groceries.router.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const pool = require('../modules/pool')
+
+const router = express.Router()
+
+
+//POST /api/groceries 
+
+//GET 
+
+
+//DELETE
+
+//PUT 
+
+
+module.exports = router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
